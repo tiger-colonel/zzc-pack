f78f075c35768d394a34fa6f8d4c8e574202c433
zhaocheng.zhai <zzcheng.me@gmail.com>
2020-11-15 10:50:51 +0800

    update: 1115

--- bin/zzc-pack.js
+++ bin/zzc-pack.js
@@ -1,3 +1,12 @@
 #! /usr/bin/env node
 
-console.log('start')
+console.log('start11');
+// 1. 需要找到当前执行名的路径， 拿到webpack.config.js
+const path = require('path');
+
+const config = require(path.resolve('webpack.config.js'));
+
+const Complier = require('../lib/Complier');
+const complier = new Complier(config);
+// 运行编译
+complier.run()
